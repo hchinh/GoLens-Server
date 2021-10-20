2850f3aca66a29e0cb9231c592ee6967990d23ce
Huynh Chinh <huynhchinhrvp10@gmail.com>
2021-10-20 11:02:06 +0700

    check

--- index.js
+++ index.js
@@ -28,5 +28,5 @@ server.use((req, res, next) => {
 // Use default router
 server.use('/api', router);
 server.listen(3000, () => {
-  console.log('JSON Server is running');
+  console.log('Server is running');
 });
